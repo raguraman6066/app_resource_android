e67a973b88cfae1ee3a01b2cb953a438d00dae9e
ragu raman <raguraman.mps6066@gmail.com>
2024-02-05 22:02:00 +0530

    initial commit

---
+++ app/src/main/java/com/example/app_resources/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.app_resources;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId=item.getItemId();
+        if(itemId==R.id.action_home){
+            Toast.makeText(this, "You Selected Home", Toast.LENGTH_SHORT).show();
+        }else if(itemId==R.id.action_search){
+            Toast.makeText(this, "You Selected Search", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
